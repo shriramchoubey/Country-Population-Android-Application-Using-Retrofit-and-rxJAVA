fcc2da8b86091f0c344489ba7d87f0c7c070964c
shriramchoubey <src.work@gmail.com>
2018-03-03 02:14:52 +0530

    This is with the function of opening the file even after saving it to the sdcard

--- app/src/main/java/io/shriram/country/ReadWrite/ReadWrite.java
+++ app/src/main/java/io/shriram/country/ReadWrite/ReadWrite.java
@@ -1,16 +1,23 @@
 package io.shriram.country.ReadWrite;
 
 
+import android.content.Context;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import org.apache.commons.io.FileUtils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -23,21 +30,22 @@ import static io.reactivex.BackpressureStrategy.BUFFER;
  *
  */
 
-public class ReadWrite {
-    public static void writeItems(String s,String filename_with_xtention,File dir) {
+public class ReadWrite extends AppCompatActivity {
+    private String filenameInternal, filenameExternal;
 
-        File filesDir = dir;
-        File todoFile = new File(filesDir,filename_with_xtention);
+    public void writeItems(String s,File file) {
+
+        File filesDir = file;
         try {
-
-
-            FileUtils.writeStringToFile(todoFile ,s);   // TODO: add depenencies for fill utils
+            FileWriter writer = new FileWriter(filesDir);
+            writer.write(s);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static File readItems(String filename_with_xtention,File dir)  {
+    public File readItems(String filename_with_xtention,File dir)  {
 
         File filesDir = dir;
         File todoFile = new File(filesDir,filename_with_xtention);
@@ -50,7 +58,7 @@ public class ReadWrite {
     }
 
     //used to zip file
-    public static void zip(File[] _files, String zipFileName) {
+    public void zip(File[] _files, String zipFileName) {
         int BUFFER = 100000; // means you can create Buffer of 10mb at a time
         try {
             BufferedInputStream origin = null;
@@ -79,4 +87,78 @@ public class ReadWrite {
             e.printStackTrace();
         }
     }
+
+    public void writeFileInternalStorage( String s, String filenameExternal) {
+        createUpdateFile(filenameInternal, s, false);
+    }
+
+    public void appendFileInternalStorage(String s, String filenameExternal) {
+        createUpdateFile(filenameInternal, s, true);
+    }
+
+    private void createUpdateFile(String fileName, String content, boolean update) {
+        FileOutputStream outputStream;
+
+        try {
+            if (update) {
+                outputStream = openFileOutput(fileName, Context.MODE_APPEND);
+            } else {
+                outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            }
+            outputStream.write(content.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readFileInternalStorage(String view) {
+        String s=null;
+        try {
+            FileInputStream fileInputStream = openFileInput(filenameInternal);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+            StringBuffer sb = new StringBuffer();
+            String line = reader.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                line = reader.readLine();
+            }
+
+            s = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+
+    public void createTemporaryFile(View view) {
+        try {
+            String fileName = "couponstemp";
+            String coupons = "Get upto 50% off shoes @ xyx shop \n Get upto 80% off on shirts @ yuu shop";
+
+            File file = File.createTempFile(fileName, null, getCacheDir());
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(coupons.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (IOException e) {
+        }
+    }
+
+    public void deleteFile(View view) {
+        try {
+            String fileName = "couponstemp";
+            File file = File.createTempFile(fileName, null, getCacheDir());
+
+            file.delete();
+        } catch (IOException e) {
+        }
+    }
+
 }
